bc89679d64281517f04ab246a486f86555e7b8c4
Monty Taylor <mordred@inaugust.com>
2011-08-10 14:35:12 -0400

    Reject commit subjects over 50 characters.
    Add a test in receive commits for commit message first lines of over 50
    characters and a boolean config option to enable checking for it.
    
    Change-Id: I557d1ff72e6df97b991b05c36483c13873b706fb

--- gerrit-sshd/src/main/java/com/google/gerrit/sshd/commands/CreateProjectCommand.java
+++ gerrit-sshd/src/main/java/com/google/gerrit/sshd/commands/CreateProjectCommand.java
@@ -75,6 +75,9 @@ final class CreateProjectCommand extends BaseCommand {
   @Option(name = "--use-content-merge", usage = "allow automatic conflict resolving within files")
   private boolean contentMerge;
 
+  @Option(name = "--require-short-message", usage = "if short commit subject is required")
+  private boolean requireShortMessage;
+
   @Option(name = "--require-change-id", aliases = {"--id"}, usage = "if change-id is required")
   private boolean requireChangeID;
 
@@ -134,6 +137,7 @@ final class CreateProjectCommand extends BaseCommand {
             args.contributorAgreements = contributorAgreements;
             args.signedOffBy = signedOffBy;
             args.contentMerge = contentMerge;
+            args.shortMessageRequired = requireShortMessage;
             args.changeIdRequired = requireChangeID;
             args.branch = branch;
             args.createEmptyCommit = createEmptyCommit;
