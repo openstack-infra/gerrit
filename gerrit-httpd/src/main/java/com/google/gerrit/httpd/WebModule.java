91f03e326f59ca80e0e116401d8f1d9d970887e6
James E. Blair <james.blair@rackspace.com>
2011-12-21 18:12:26 +0000

    Add OpenID SSO support.
    Configuring OPENID_SSO in gerrit.config will allow the admin
    to specify an SSO entry point URL so that users clicking on
    "Sign In" are sent directly to that URL.
    
    Change-Id: I361b7b74a6f34a199578620540be31d1df0c5fef

--- gerrit-httpd/src/main/java/com/google/gerrit/httpd/WebModule.java
+++ gerrit-httpd/src/main/java/com/google/gerrit/httpd/WebModule.java
@@ -106,6 +106,7 @@ public class WebModule extends FactoryModule {
         break;
 
       case OPENID:
+      case OPENID_SSO:
         // OpenID support is bound in WebAppInitializer and Daemon.
       case CUSTOM_EXTENSION:
         break;
