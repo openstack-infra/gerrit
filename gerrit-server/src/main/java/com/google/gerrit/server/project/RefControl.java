5c71543716b22a11365961180271058403bc4f83
Khai Do <zaro0508@gmail.com>
2013-10-11 18:14:31 +0200

    Add a Change Owner group
    Add a new system 'CHANGE_OWNER' (the patch set owner) group to allow
    configuration of this group with gerrit label permissions.
    
    To keeps it consistent with how the existing system groups
    (Project Owner, Admin, etc..) are configured the Change Owner
    group's logic for existing change context permissions like
    Abandon, Rebase, View Drafts, etc. is untouched by this patch.
    In other words an Admin can only apply this groups to a label
    permission.
    
    From upstream: https://gerrit-review.googlesource.com/#/c/48254
    Bug: issue 1993
    Change-Id: Ibb35b8172ce50319cfc2fca3b661fdaf7e245e5d

--- gerrit-server/src/main/java/com/google/gerrit/server/project/RefControl.java
+++ gerrit-server/src/main/java/com/google/gerrit/server/project/RefControl.java
@@ -379,14 +379,14 @@ public class RefControl {
   }
 
   /** All value ranges of any allowed label permission. */
-  public List<PermissionRange> getLabelRanges() {
+  public List<PermissionRange> getLabelRanges(boolean isChangeOwner) {
     List<PermissionRange> r = new ArrayList<PermissionRange>();
     for (Map.Entry<String, List<PermissionRule>> e : relevant.getDeclaredPermissions()) {
       if (Permission.isLabel(e.getKey())) {
         int min = 0;
         int max = 0;
         for (PermissionRule rule : e.getValue()) {
-          if (projectControl.match(rule)) {
+          if (projectControl.match(rule, isChangeOwner)) {
             min = Math.min(min, rule.getMin());
             max = Math.max(max, rule.getMax());
           }
@@ -401,8 +401,13 @@ public class RefControl {
 
   /** The range of permitted values associated with a label permission. */
   public PermissionRange getRange(String permission) {
+    return getRange(permission, false);
+  }
+
+  /** The range of permitted values associated with a label permission. */
+  public PermissionRange getRange(String permission, boolean isChangeOwner) {
     if (Permission.hasRange(permission)) {
-      return toRange(permission, access(permission));
+      return toRange(permission, access(permission, isChangeOwner));
     }
     return null;
   }
@@ -527,6 +532,12 @@ public class RefControl {
 
   /** Rules for the given permission, or the empty list. */
   private List<PermissionRule> access(String permissionName) {
+    return access(permissionName, false);
+  }
+
+  /** Rules for the given permission, or the empty list. */
+  private List<PermissionRule> access(String permissionName,
+      boolean isChangeOwner) {
     List<PermissionRule> rules = effective.get(permissionName);
     if (rules != null) {
       return rules;
@@ -540,7 +551,7 @@ public class RefControl {
     }
 
     if (rules.size() == 1) {
-      if (!projectControl.match(rules.get(0))) {
+      if (!projectControl.match(rules.get(0), isChangeOwner)) {
         rules = Collections.emptyList();
       }
       effective.put(permissionName, rules);
@@ -549,7 +560,7 @@ public class RefControl {
 
     List<PermissionRule> mine = new ArrayList<PermissionRule>(rules.size());
     for (PermissionRule rule : rules) {
-      if (projectControl.match(rule)) {
+      if (projectControl.match(rule, isChangeOwner)) {
         mine.add(rule);
       }
     }
